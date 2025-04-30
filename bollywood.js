5e2c3a5a614d39708f091ca578010d1e423c9caf
manjilaryal546 <manjilaryal546@gmail.com>
2025-04-30 19:06:24 +0545

    Update bollywood.js

--- bollywood.js
+++ bollywood.js
@@ -1,8 +1,41 @@
 function findB(){
    let Bsearch = document.getElementById("bsearch").value;
    switch(Bsearch){
-      case "Gharjwai":
-         window.location.href ="gharjwai.html";
+      case "Animal":
+         window.location.href ="animal.html";
+      break;
+          case "Deva":
+         window.location.href ="deva.html";
+      break;
+          case "Kaithi":
+         window.location.href ="kaithi.html";
+      break;
+          case "KGF : Chapter 1":
+         window.location.href ="khf1.html";
+      break;
+          case "KGF : Chapter 2":
+         window.location.href ="kgf2.html";
+      break;
+          case "Leo":
+         window.location.href ="leo.html";
+      break;
+          case "Live":
+         window.location.href ="live.html";
+      break;
+          case "Marco":
+         window.location.href ="marco.html";
+      break;
+          case "RA.ONE":
+         window.location.href ="raone.html";
+      break;
+          case "Salaar":
+         window.location.href ="salaar.html";
+      break;
+          case "Sikandar":
+         window.location.href ="sikandar.html";
+      break;
+          case "Vikram":
+         window.location.href ="vikram.html";
       break;
       default:
          break;
