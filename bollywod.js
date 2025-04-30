5be820d76e1c029b094fa1fc8bc059cdf8bb400f
manjilaryal546 <manjilaryal546@gmail.com>
2025-04-30 14:44:26 +0545

    Add files via upload

---
+++ bollywod.js
@@ -0,0 +1,10 @@
+function findB(){
+   let Bsearch = document.getElementById("bsearch").value;
+   switch(Bsearch){
+      case "Gharjwai":
+         window.location.href ="detail.html";
+      break;
+      default:
+         break;
+   }
+}
---
+++ kollywood.js
@@ -0,0 +1,51 @@
+
+function findK(){
+    let Ksearch = document.getElementById("ksearch").value;
+    switch(Ksearch){
+       case "Gharjwai":
+          window.location.href ="gharjwai.html";
+       break;
+       case "Bato":
+          window.location.href ="bato.html";
+       break;
+          case "Chadke":
+          window.location.href ="chadke.html";
+       break;
+          case "Chadke 2":
+          window.location.href ="chadke2.html";
+       break;
+          case "Dal Bhat Tarkari":
+          window.location.href ="dalbhattarkari.html";
+       break;
+          case "Degree Maila":
+          window.location.href ="degreemaila.html";
+       break;
+          case "Dui Numbari":
+          window.location.href ="duinumbari.html";
+       break;
+          case "Fullbari":
+          window.location.href ="fullbari.html";
+       break;
+          case "Guleli":
+          window.location.href ="guleli.html";
+       break;
+          case "Jaari":
+          window.location.href ="jaari.html";
+       break;
+          case "Maha Purush":
+          window.location.href ="mahapurush.html";
+       break;
+          case "Modibeni":
+          window.location.href ="modibeni.html";
+       break;
+          case "Nango Gaun":
+          window.location.href ="nangogaun.html";
+       break;
+          case "Prasad":
+          window.location.href ="prasad.html";
+       break;
+       default:
+          break;
+    }
+ }
+ 
